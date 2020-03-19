decadff32d7d01e745d53034c33ff5bb39513c07
Michael Bassili <mbassili@sfu.ca>
2020-03-19 16:10:41 -0700

    Add shell, add intro messages

---
+++ shell.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+"""Interactive shell for SimpleScript programming language"""
+
+title_note = "SimpleScript: Interpreted Programming Language"
+copyright_note = "(c) 2020 Michael Bassili Licensed Under GPL-3.0"
+print(title_note + '\n' + copyright_note + '\n')
+
+while True:
+    arg = input('[SimpleScript shell]$ ')
+    print(arg)
