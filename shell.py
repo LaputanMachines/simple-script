e1fbbfc2a44eba6ff6c07428f3de034db008940b
Michael Bassili <mbassili@sfu.ca>
2020-03-22 15:59:34 -0700

    Add debug mode to interactive shell

--- shell.py
+++ shell.py
@@ -3,6 +3,16 @@
 
 import simplescript
 
+##################
+# TERMINAL FLAGS #
+##################
+
+print_errors = False
+
+###################
+# WELCOME MESSAGE #
+###################
+
 hash_divider = '###################################################'
 title_note = "# SimpleScript: Interpreted Programming Language  #"
 copyright_note = "# (c) 2020 Michael Bassili Licensed Under GPL-3.0 #"
@@ -11,9 +21,17 @@ print('\n' + hash_divider + '\n' + title_note + '\n'
 
 while True:
     input_stream = input('$ ')
+
+    # Special terminal commands
     if input_stream == 'exit':
         print()  # Newline to separate content
         exit(0)  # Terminate from the shell
+    elif input_stream == 'debug':
+        print_errors = not print_errors
+        continue
+
     result, error = simplescript.run('<stdin>', input_stream)
     if result:
         print(result)
+    if error and print_errors:
+        print(error)
